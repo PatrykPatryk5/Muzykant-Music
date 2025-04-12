394c58eb322e493e1e7cba66f99c1794e5a8de1e
PatrykPatryk5 <128894397+PatrykPatryk5@users.noreply.github.com>
2025-04-12 10:41:29 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,3 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-
 export default [
     {
         ignores: [
@@ -42,6 +26,8 @@ export default [
             "@typescript-eslint/no-explicit-any": 1,
             "@typescript-eslint/no-unsafe-declaration-merging": 1,
             "@typescript-eslint/no-unused-vars": 1,
+            // Wyłączamy regułę no-require-imports
+            "@typescript-eslint/no-require-imports": "off",
         },
     }
 ];
